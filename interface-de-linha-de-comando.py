a6ffff529a8cbce7526fcd69bdf75f73945c4ef7
Julia Valadares <ju.valadares12@gmail.com>
2023-03-23 16:24:25 -0300

    Linha de comando

---
+++ interface-de-linha-de-comando.py
@@ -0,0 +1,8 @@
+from calc import converterStringParaFloat, Gigabyte, byteParaBit
+
+print('Escreva 1 para MegabyteparaGigabyte \n -Escreva 2 GigabyteparaMegabyte')
+funcEscolha = input()
+if(funcEscolha = 1)
+entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+valorConvertido = byteParaBit(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
